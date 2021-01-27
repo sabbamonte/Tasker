95f371b2029f5536c287d34b83cb54f726de0abe
Simone Abbamonte <simone@Simones-MacBook-Air.local>
2021-01-27 17:14:46 -0500

    changed webdriver options

--- Tasker/tests.py
+++ Tasker/tests.py
@@ -12,7 +12,12 @@ from selenium import webdriver
 def file_uri(filename):
     return pathlib.Path(os.path.abspath(filename)).as_uri()
 
-driver = webdriver.Chrome()
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--window-size=1420,1080')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
+driver = webdriver.Chrome(chrome_options=chrome_options)
 
 # Create a task
 
