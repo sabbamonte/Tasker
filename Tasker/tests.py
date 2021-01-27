307cbaa9a3286a70abe70da910c60573f3361263
Simone Abbamonte <simone@Simones-MacBook-Air.local>
2021-01-27 16:49:53 -0500

    added selenium tests

--- Tasker/tests.py
+++ Tasker/tests.py
@@ -1,3 +1,39 @@
 from django.test import TestCase
+from datetime import date, datetime
+from time import sleep
+import os
+import unittest
+import pathlib
+
+from selenium import webdriver
 
 # Create your tests here.
+
+def file_uri(filename):
+    return pathlib.Path(os.path.abspath(filename)).as_uri()
+
+driver = webdriver.Chrome()
+
+# Create a task
+
+class WebpageTests(unittest.TestCase):
+
+    def test_title(self):
+        driver.get(file_uri("templates/Tasker/index.html"))
+        self.assertEqual(driver.title, "")
+
+    def test_archive(self):
+        driver.get(file_uri("templates/Tasker/index.html"))
+        task = driver.find_element_by_class_name("checkmark")
+        task.click()
+        self.assertTrue(task.is_selected, True)
+
+    def test_archive_site(self):
+        driver.get(file_uri("templates/Tasker/archive.html"))
+        archive = driver.find_element_by_name("delete_task")
+        archive.click()
+        self.assertTrue(archive.is_selected, True)
+
+if __name__ == "__main__":
+    unittest.main()
+
