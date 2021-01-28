1bbec0470bb6e9ec098a98c09b77f6a25693b1ea
Simone Abbamonte <simone@Simones-MacBook-Air.local>
2021-01-28 10:45:32 -0500

    f

--- tests.py
+++ tests.py
@@ -6,18 +6,14 @@ import unittest
 import pathlib
 
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from chromedriver_py import binary_path
 
 # Create your tests here.
 
 def file_uri(filename):
     return pathlib.Path(os.path.abspath(filename)).as_uri()
 
-chrome_options = Options()
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome(executable_path='/usr/local/Caskroom/chromedriver/88.0.4324.96/chromedriver.exe',chrome_options=chrome_options)
+driver = webdriver.Chrome(executable_path=binary_path)
 
 # Create a Testss
 
