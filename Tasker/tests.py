311cf7a1552d21bfe67d1dc5237255f67f839aec
Simone Abbamonte <simone@Simones-MacBook-Air.local>
2021-01-28 10:00:40 -0500

    changed webdriver options

--- Tasker/tests.py
+++ Tasker/tests.py
@@ -14,10 +14,10 @@ def file_uri(filename):
     return pathlib.Path(os.path.abspath(filename)).as_uri()
 
 chrome_options = Options()
-chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome('/home/PycharmProjects/chromedriver',chrome_options=chrome_options)
+driver = webdriver.Chrome('/usr/local/bin/chromedriver',chrome_options=chrome_options)
 driver.get('https://www.google.com/')
 
 # Create a task
